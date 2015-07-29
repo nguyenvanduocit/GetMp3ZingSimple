2b4678d766ed4de91ebc0b9967220984ef85696e
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-07-30 00:45:28 +0700

    Get pagecontent

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
  * @author nguyenvanduocit
  */
 require_once __DIR__.'/vendor/autoload.php';
+global $client;
 
 $url = 'http://mp3.zing.vn/bai-hat/Khuon-Mat-Dang-Thuong-Son-Tung-M-TP/ZW70UUZF.html';
 $type = getType($url);
+$content = getPageContent($url);
 
 function getType($url){
 	$regex = "/http:\\/\\/mp3\\.zing\\.vn\\/(bai-hat|album|video-clip|playlist)\\/(?:.*)/";
@@ -18,5 +20,20 @@ function getType($url){
 	}
 	else{
 		return FALSE;
+	}
+}
+
+function getPageContent($url)
+{
+	global $client;
+	if(!$client) {
+		$client = new GuzzleHttp\Client();
+	}
+	$res = $client->get($url);
+	if($res->getStatusCode() === 200){
+		return $res->getBody()->getContents();
+	}
+	else{
+		return null;
 	}
 }
